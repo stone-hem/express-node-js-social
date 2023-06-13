29328e12f5452ca0c593cd16ce3e806032205eb1
Hemstone <hemstone456@gmail.com>
2023-06-13 11:16:07 +0300

    auth

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from './routes/auth.js'
+import { register } from "./controllers/auth.js";
 
 //configurations
 const __filename=fileURLToPath(import.meta.url);
@@ -36,14 +38,21 @@ const storage=multer.diskStorage({
 
 const upload=multer({storage});
 
+//routes
+app.post("/auth/register",upload.single('picture'),register);
+
+//routes
+app.use("/auth",authRoutes)
+
+
 //mongoose setup
-const PORT=process.env.PORT||3500;
+const PORT=process.env.PORT;
 
 mongoose.connect(process.env.DATABASE_URL,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
-    app.listen(PORT,()=>console.log(`Server running on: http:\\localhost:${PORT}`))
+    app.listen(PORT,()=>console.log(`Server Running on Port http://localhost:${PORT}`))
 }).catch((error)=>{
-    console.log(`${error} not connected`)
+    console.log(`${error} not connected`) 
 })
